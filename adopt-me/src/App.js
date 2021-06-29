4280c797c7e0cfb4ac5e136a4b2b5dc14f006a99
ibrahim <dnzibrahim1@gmail.com>
2021-06-29 21:30:01 +0300

    Add Core React Concepts : Strict Mode section

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
 
@@ -11,4 +11,9 @@ const App = () => {
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
