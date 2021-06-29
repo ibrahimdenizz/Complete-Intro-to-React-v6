c8ba5bfc87236c8643106cbfb2a78716339916b5
ibrahim <dnzibrahim1@gmail.com>
2021-06-29 22:11:22 +0300

    Add React Capabilities : React Router Link Tag section

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
+      <header>
+        <Link to="/">
+          <h1>Adopt Me!</h1>
+        </Link>
+      </header>
       <Router>
         <Switch>
           <Route path="/details/:id">
