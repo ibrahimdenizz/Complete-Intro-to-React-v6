1adb1aac231005213e9358a2e791c45628ad2e02
ibrahim <dnzibrahim1@gmail.com>
2021-06-30 12:35:02 +0300

    Add Special Case React Tools : Error Boundaries section

---
+++ adopt-me/src/ErrorBoundary.js
@@ -0,0 +1,25 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError() {
+    // I log this to Sentry, Azure Monitor, New Relic, TrackJS
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h2>
+          This listing is has an error. <Link to="/">Click here</Link> to go
+          back to the home page.
+        </h2>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
