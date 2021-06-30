605b21c7eeea1f897f7cde872f91bc9edd28946d
ibrahim <dnzibrahim1@gmail.com>
2021-06-30 12:44:19 +0300

    Add Special Case React Tools : Redirect on Error section

--- adopt-me/src/ErrorBoundary.js
+++ adopt-me/src/ErrorBoundary.js
@@ -5,12 +5,19 @@ class ErrorBoundary extends Component {
   state = { hasError: false };
 
   static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
     // I log this to Sentry, Azure Monitor, New Relic, TrackJS
     console.error("ErrorBoundary caught an error", error, info);
+    setTimeout(() => this.setState({ redirect: true }), 5000);
   }
 
   render() {
-    if (this.state.hasError) {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    } else if (this.state.hasError) {
       return (
         <h2>
           This listing is has an error. <Link to="/">Click here</Link> to go
