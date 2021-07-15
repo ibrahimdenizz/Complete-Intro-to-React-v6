79573242eac7e1aa76b62f2f6040e1ff4ed3c54f
ibrahim <dnzibrahim1@gmail.com>
2021-07-15 14:12:48 +0300

    Add TypeScript : TypeScript Refactoring section

---
+++ adopt-me/src/Modal.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef, FunctionComponent, MutableRefObject } from "react";
+import { createPortal } from "react-dom";
+
+// let modalRoot;
+const modalRoot = document.getElementById("modal");
+
+const Modal: FunctionComponent = ({ children }) => {
+  // modalRoot = modalRoot ? modalRoot : document.getElementById("modal");
+
+  const elRef : MutableRefObject<HTMLDivElement | null> = useRef(null);
+  
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    if(!modalRoot || !elRef.current){
+      return;
+    }
+
+    modalRoot.appendChild(elRef.current);
+    return () => {
+      if(elRef.current)
+      modalRoot.removeChild(elRef.current)
+    };
+  }, []);
+
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
